c373d23a97f9596b6e6a9010ca739795702cc333
vvv-hikka <63813038+vvv-hikka@users.noreply.github.com>
2023-12-13 22:51:17 +0300

    Update general.py

--- general.py
+++ general.py
@@ -3,6 +3,7 @@ import pandas as pd
 import plotly.express as px
 import requests
 import numpy as np
+import io
 
 st.title('Alcohol Consumption in Russia (1998 - 2016)')
 st.header('The dataset includes information on consumption of five different types of alcohol (beer, wine, vodka, brandy, champagne) in different regions of Russia over the period betwenn 1998 and 2016')
@@ -17,4 +18,7 @@ st.header('Data Cleanup')
 st.code(r'''df = pd.read_csv('russia_alcohol.csv')
 df.info()''')
 df = pd.read_csv('russia_alcohol.csv
-st.write(st.info())
+buffer = io.StringIO()
+df.info(buf=buffer)
+s = buffer.getvalue()
+st.text(s)
